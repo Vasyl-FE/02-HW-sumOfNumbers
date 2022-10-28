4909fc1a8e381a87d28eebea03166693c69280d2
Vasyl S <sukhovoy.v@gmail.com>
2022-10-28 20:05:16 +0300

    upload HW

---
+++ index.js
@@ -0,0 +1,10 @@
+const num1 = Number(prompt("So, you want to add two numbers. Please, enter first number below:"));
+const num2 = Number(prompt("Enter second number below:"));
+const result = num1 + num2;
+
+alert(`${num1} + ${num2} = ${result}`);
+
+console.log(typeof num1);
+console.log(typeof num2);
+console.log(typeof result);
+
